e15ca6c83319f47c06dbb891b2a272a81505635f
Piotr Kluczny <piotrkluczny@europarts.pl>
2023-06-12 21:34:49 +0200

    Using fluentassertion packegs

--- MyProcject.Tests/BmiCalculatorFacadeTests.cs
+++ MyProcject.Tests/BmiCalculatorFacadeTests.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+
 
 namespace MyProcject.Tests
 {
@@ -27,11 +29,15 @@ namespace MyProcject.Tests
 
             //assert
 
-            Assert.Equal(24.93, result.Bmi);
-            Assert.Equal(BmiClassification.Overweight, result.BmiClassification);
-            Assert.Equal(overweightSummaryResult, result.Summary);
+            //Assert.Equal(24.93, result.Bmi);
+            //Assert.Equal(BmiClassification.Overweight, result.BmiClassification);
+            //Assert.Equal(overweightSummaryResult, result.Summary);
 
+            // Uzycie paczki fFuentAssertions aby troche uprościć cześć ASSERT
 
+            result.Bmi.Should().Be(24.93);
+            result.BmiClassification.Should().Be(BmiClassification.Overweight);
+            result.Summary.Should().Be(overweightSummaryResult);
 
 
         }
